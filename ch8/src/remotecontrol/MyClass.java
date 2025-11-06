eb1709adf9890da2ef4c5bbabc3080e2d72c8aa4
0sun <dladudtjs9@gmail.com>
2025-11-06 20:21:46 +0900

    11.6

---
+++ ch8/src/remotecontrol/MyClass.java
@@ -0,0 +1,13 @@
+package remotecontrol;
+
+public class MyClass {
+//	MyClass(RemoteControl rc){
+//		rc.TurnOn();
+//		rc.SetVolume(10);
+//	}
+
+void MethodB(RemoteControl rc){
+	rc.TurnOn();
+	rc.SetVolume(2);
+}
+}
